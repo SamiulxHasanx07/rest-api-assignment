ed020955110c9a59c378360cc40a5a630a26fb57
SamiulxHasanx07 <samiulxhasan650@gmail.com>
2022-09-03 20:44:17 +0600

    Heroku server error

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const user = require('./user.json');
 const app = express();
 
